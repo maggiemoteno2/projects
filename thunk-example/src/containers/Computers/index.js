8ed191a42396740f96d3fd22626954d484f02838
TCGPC1 <you@example.com>
2019-10-18 10:06:39 +0200

    deleted codes that i don't use

--- thunk-example/src/containers/Computers/index.js
+++ thunk-example/src/containers/Computers/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { removeComputer } from "./../../redux/actions";
-import { addComputer } from "./../../redux/actions";
 import moment from "moment";
 
 
