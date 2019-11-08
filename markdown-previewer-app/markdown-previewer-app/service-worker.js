06b31d2e31e89514e8e208cbc6cd9245f3f03388
maggiemoteno2 <motenomaggie@gmail.com>
2019-11-08 11:21:54 +0200

    project done

--- markdown-previewer-app/markdown-previewer-app/service-worker.js
+++ markdown-previewer-app/markdown-previewer-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be921b7be3896201a699c31b20937a53.js"
+  "/maggiemoteno2/maggiemoteno.github.io/tree/master/free-code-camp-projects/markdown-previewer-app/precache-manifest.45501bcf19583c5ba1f3ab9616e78bec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maggiemoteno2/maggiemoteno.github.io/tree/master/free-code-camp-projects/markdown-previewer-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
